14b3be84c1729103fb484ee912fe06524348265a
ngo.dinh.ngoc <ngo.dinh.ngoc@framgia.com>
2018-02-23 15:05:42 +0700

    add custom facade

---
+++ app/Helpers/FramgiaUtil.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers;
+
+class FramgiaUtil
+{
+    /**
+     * @return string
+     */
+    function getUsername()
+    {
+        return 'FramgiaFacade report 201802';
+    }
+}
